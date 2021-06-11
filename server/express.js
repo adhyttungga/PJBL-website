cf59addba72322325a967bed7cebd567122adb1f
adhyttungga <adhyttungga.jkt@gmail.com>
2021-06-11 15:40:12 +0700

    updating server side

---
+++ server/express.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compress from 'compression'
+import cors from 'cors'
+import helmet from 'helmet'
+import Template from './../template.js'
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ exended: true }))
+app.use(cookieParser())
+app.use(compress())
+app.use(cors())
+app.use(helmet())
+
+app.get('/', (req, res) => {
+  res.status(200).send(Template())
+})
+
+
+
+
+
+
+
+
+export default app
