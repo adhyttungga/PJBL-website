8cb51e89a208c17677ff2447da90a559e4378736
adhyttungga <adhyttungga.jkt@gmail.com>
2021-07-05 16:48:21 +0700

    update server.js

--- server/express.js
+++ server/express.js
@@ -5,15 +5,24 @@ import compress from 'compression'
 import cors from 'cors'
 import helmet from 'helmet'
 import Template from './../template.js'
+import userRoutes from './../routes/user.routes'
+import authRoutes from  './../routes/auth.routes'
 
 const app = express()
 
+// parse body params and attache them to req.body
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ exended: true }))
 app.use(cookieParser())
 app.use(compress())
+// enable CORS - Cross Origin Resource Sharing
 app.use(cors())
+// secure apps by setting various HTTP headers
 app.use(helmet())
+
+// mount routes
+app.use('/', userRoutes)
+app.user('/', authRoutes)
 
 app.get('/', (req, res) => {
   res.status(200).send(Template())
